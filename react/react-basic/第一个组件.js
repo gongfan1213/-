57e3a8efd80c4f043a16ac4fbb91bc6d1c22c3da
gongfan1213 <gongfan1213@163.com>
2025-03-06 14:05:46 +0800

    fix: 第一个组件

---
+++ react/react-basic/第一个组件.js
@@ -0,0 +1,9 @@
+//react组件时一段可以使用标签进行扩展的javascript函数
+//react组件时一段可以使用标签进行扩展的javascript函数
+//组件可以渲染其他组件，不要嵌套他们的定义
+//react允许你创建组件，应用程序的可复用ui元素
+//在react应用程序当中，每一个ui模块都是组件
+//react是一个常规的javasceipt函数，他们的名字总是以大写字母开始的，返回jsx标签
+//react组件的名字必须是以大写字母开头的，
+//不可以在组件当中定义组件的
+//https://react.docschina.org/learn/importing-and-exporting-components
